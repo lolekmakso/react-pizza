a38e0cc544de8dfb378327ddde47c6faa5827dc0
Maksym <makstarasovv1@gmail.com>
2025-02-20 21:52:26 +0200

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import { NotFound } from "./pages/NotFound";
 import { Routes, Route } from "react-router";
 
 import "./scss/app.scss";
+import { useState } from "react";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <div className="wrapper">
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
